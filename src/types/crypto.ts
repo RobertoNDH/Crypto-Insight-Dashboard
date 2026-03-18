e8e57f6c85990ae0686798054e409f8d3436a57c
RobertoNDH <robertonauzet@gmail.com>
2026-03-18 23:17:29 +0000

    chore: initial commit with vite vanilla-ts template

---
+++ src/types/crypto.ts
@@ -0,0 +1,17 @@
+export interface Coin {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  currentPrice: number;
+  marketCap: number;
+  priceChange24h: number; 
+}
+
+export interface PortfolioAsset extends Coin {
+  amountOwned: number;    
+  avgPurchasePrice: number; 
+  totalValueUsd: number;  
+}
+
+export type AppStatus = 'idle' | 'loading' | 'success' | 'error';
